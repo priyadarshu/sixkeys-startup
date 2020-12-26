eb64f3e61ed3071df25a22ace53257a2e37d1298
priyadarshu <priyadarshinichettiar@gmail.com>
2020-12-26 13:18:50 +0530

    Added .html files for UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import  "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Home";
+import About from "./About";
+import Service from "./Service";
+import Contact from "./Contact";
+import { Route, Switch, Redirect } from 'react-router-dom';
+
 
 const App=()=>{
     return (
         <>
-        <h1>Hello </h1>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/about" component={About}/>
+                <Route exact path="/service" component={Service}/>
+                <Route exact path="/contact" component={Contact}/>
+                <Home/>
+                <Redirect to="/" />
+            </Switch>
         </>
     );
 }
